2d833a228a500535daea4c44f21fd7fe8f2f6726
mintani <mi_gaming@outlook.jp>
2024-12-04 02:05:23 +0000

    chore: file moved

--- src/app/(editor)/comic/new/page.tsx
+++ src/app/(editor)/comic/new/page.tsx
@@ -1,3 +1,20 @@
-export default function Create() {
-  return <div className="flex flex-1 flex-col">Create</div>;
+import { Header } from "@/components/layout/header";
+import { auth } from "@/lib/auth";
+import { Noto_Sans_JP } from "next/font/google";
+import { Characterform } from "./_components/form";
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin"],
+  weight: ["300"],
+});
+const logo = "/logo.svg";
+
+export default async function Character() {
+  const session = await auth();
+  return (
+    <>
+      <Header />
+      <Characterform />
+    </>
+  );
 }
