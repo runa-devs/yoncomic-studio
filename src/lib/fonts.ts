42770fe9966b2789864095e96d7157c2950ab12b
caru-ini <caru17122@gmail.com>
2024-12-06 16:41:17 +0000

    refactor: use local font instead of google font

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,11 +1,13 @@
-import { M_PLUS_Rounded_1c, Noto_Sans_JP } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
+import localFont from "next/font/local";
 
 export const notoSansJP = Noto_Sans_JP({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
+  display: "swap",
 });
 
-export const mplus = M_PLUS_Rounded_1c({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
+export const mplus = localFont({
+  src: "./MPLUSRounded1c-Regular.ttf",
+  display: "swap",
 });
