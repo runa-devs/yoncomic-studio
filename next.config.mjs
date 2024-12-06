ad0d245de0d9828e7ead09164e0e8079305d6435
caru-ini <caru17122@gmail.com>
2024-12-06 15:33:53 +0000

    chore: allow r2 image in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: "pub-8b50a2a96a4e4d76a50ecc6edb85cd97.r2.dev" },
+    ],
+  },
+};
 
 export default nextConfig;
