8de0836b393183c6dfd0ea94531bba9e9f521def
caru-ini <caru17122@gmail.com>
2024-12-08 00:42:56 +0000

    fix: fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
     images:{
         remotePatterns:[
             { hostname: "pub-8b50a2a96a4e4d76a50ecc6edb85cd97.r2.dev"},
-            { 
-                protocol: 'https',
-                hostname: process.env.NEXT_PUBLIC_S3_PUBLIC_URL?.replace(/^https?:\/\//, ''),
-            }
         ],
     }
 };
